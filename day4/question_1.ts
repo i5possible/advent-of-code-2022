43a948293282f3a2c12a80494a475e11dfd85fdf
alfred <alfred.parallel.fi>
2022-12-05 09:39:25 +0800

    day 4

---
+++ day4/question_1.ts
@@ -0,0 +1,20 @@
+import { readLines } from "../readFile";
+const lines = readLines("./day4/input.txt");
+
+const checkOverlap = (line: string) => {
+  const [first, second] = line.split(",");
+  const [firstMin, firstMax] = first.split("-").map((s) => parseInt(s));
+  const [secondMin, secondMax] = second.split("-").map((s) => parseInt(s));
+  const hasOverlap =
+    (firstMin <= secondMin && firstMax >= secondMax) ||
+    (firstMin >= secondMin && firstMax <= secondMax);
+  console.log(
+    `hasOverlap: ${hasOverlap}, firstMin: ${firstMin} - firstMax: ${firstMax}, secondMin: ${secondMin}, secondMax: ${secondMax}`
+  );
+  return hasOverlap;
+};
+const result = lines.filter((line) => checkOverlap(line)).length;
+
+console.log(result);
+
+// 547
