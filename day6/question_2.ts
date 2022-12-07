f01819e9639d3ec9a15c9c0e71c31b42167c161c
alfred <alfred.parallel.fi>
2022-12-07 13:19:33 +0800

    add day6

---
+++ day6/question_2.ts
@@ -0,0 +1,21 @@
+import { readData } from "./../readFile";
+const dataStream = readData("./day6/input.txt")!;
+
+console.log(dataStream);
+
+let distinctCharacters = "";
+for (let i = 0; i < dataStream.length; i++) {
+  const character = dataStream[i];
+  if (distinctCharacters.includes(character)) {
+    const index = distinctCharacters.indexOf(character);
+    distinctCharacters = distinctCharacters.slice(index + 1);
+  }
+  distinctCharacters += character;
+  console.log(`i: ${i}, distinctCharacters: ${distinctCharacters}`);
+  if (distinctCharacters.length === 14) {
+    console.log(`index: ${i + 1}`);
+    break;
+  }
+}
+
+// 2472
